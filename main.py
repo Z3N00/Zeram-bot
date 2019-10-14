330ea4b89805a33b78f9f1fc31f8ee89cf3fd7ff
Kartik Gupta <kartikgupta676@gmail.com>
2019-10-14 21:54:46 +0530

    testing

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from discord.ext import commands
 import sys, traceback
 
 token = 'MJ-Wjxz7u4NRQRJcQsTVG9Ff5hA.g8FAVX.zYTN0UDM4YjM2MzMxEjN3AjN'
-initial_extensions = ['cogs.reply',
+initial_extensions = ['cogs.fun',
                       'cogs.basic',
-                      'cogs.fun',
+                      'cogs.reply',
                       'cogs.jokes',
                       'cogs.utility',
                       'cogs.moderation']
@@ -20,8 +20,8 @@ if __name__ == '__main__':
 @bot.event
 async def on_ready():
     guild = len(bot.guilds)
-    activity = discord.Activity(name=str(guild) + 'servers', type=discord.ActivityType.watching)
-    await bot.change_presence(activity=activity)
+    activity = discord.Activity(name='z.help | ' + str(guild) + ' servers', type=discord.ActivityType.listening)
+    await client.change_presence(activity=activity)
 
     print(f'\n\nLogged in as: {bot.user.name} - {bot.user.id}\nVersion: {discord.__version__}\n')
     print(f'Successfully logged in and booted...!')
